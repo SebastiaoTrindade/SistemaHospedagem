05d5876be281041275cd3fc80a70c743dba46a02
sebastião oliveira trindade <sebatrindade@hotmail.com>
2022-10-24 11:59:20 -0300

    Add project files.

---
+++ Models/Reserva.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+
+        public Reserva(Suite suite)
+        {
+            this.Suite = suite;
+        }
+
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {            
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Números de hóspede superior a capacidade da suíte");
+            }
+            
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = 0;
+
+            valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 100)
+            {
+                valor -= valor * 10 / 100;
+            }
+
+            return valor;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using SistemaHospedagem.Models;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+// Cria modelos de hóspede e cadastra
+List<Pessoa> hospede = new List<Pessoa>();
+
+Pessoa pessoa1 = new Pessoa("Hóspede 1");
+Pessoa pessoa2 = new Pessoa("Hóspede 2");
+
+hospede.Add(pessoa1);
+hospede.Add(pessoa2);
+
+// Insere uma suíte
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 50);
+
+// Cria uma nova reserva
+Reserva reserva = new Reserva(diasReservados: 10);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospede);
+
+// Exibe reserva completa
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
